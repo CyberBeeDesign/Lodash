ebbec30122b3c4c8e70facf8c26f6d4741e675bf
CyberBeeDesign <cyberbeedesign@gmail.com>
2024-12-27 16:15:48 +0100

    InRange Method

--- lodash.js
+++ lodash.js
@@ -1,14 +1,32 @@
 const _ = {
     
-//clamp()
+    //clamp()
     clamp (number, lower, upper) {
         const lowerClampedValue = Math.max(number, lower);
         const clampedValue = Math.min(upper, lowerClampedValue);
     return clampedValue;
+    },
+
+    //range()
+    inRange (number, start, end) {
+        if(!end){
+            end = start;
+            start = 0;
+        } 
+        
+        if (start > end){
+            const temp = end;
+            end = start;
+            start = temp;
+        }
+
+        const isInRange = start <= number && number < end;
+        return isInRange;
     }
 }
 
-console.log(_.clamp(12, -25, 36))
+//console.log(_.clamp(12, -25, 36))
+console.log(_.inRange(.10, 20, 30))
 
 
 
